5493b6e055203861ce7f888b03bbcb8d11d484b8
Christian Macher <christian.macher@openforce.com>
2015-09-16 13:40:02 +0200

    Added SourceUtil to read optional sources from file.

--- app/util/SourceUtil.scala
+++ app/util/SourceUtil.scala
@@ -1,5 +1,22 @@
 package util
 
-class SourceUtil {
+import scala.io.{Source, BufferedSource}
+
+object SourceUtil {
+
+  def readSourceOpt[T](name: String)(handler: Option[BufferedSource] => T): T = {
+    val maybeBufferedSource = try {
+      Some(Source.fromFile(name))
+    } catch {
+      case e: Exception => None
+    }
+    maybeBufferedSource.map { bs =>
+      try {
+        handler(Some(bs))
+      } finally {
+        bs.close()
+      }
+    } getOrElse { handler(None)}
+  }
 
 }
