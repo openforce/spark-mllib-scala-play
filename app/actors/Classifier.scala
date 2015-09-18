3a72cc9b0bdf988734e6d7cee8219259ace485ff
Shokunin San <me@flatmap.io>
2015-09-18 11:20:27 +0200

    Use defaults for pipeline stages.

--- app/actors/Classifier.scala
+++ app/actors/Classifier.scala
@@ -6,6 +6,7 @@ import akka.actor.{ActorRef, Actor, Props}
 import akka.util.Timeout
 import models.CorpusItem
 import org.apache.spark.SparkContext
+import org.apache.spark.ml.param.{ParamPair, ParamMap}
 import org.apache.spark.ml.{Model, Pipeline}
 import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
@@ -75,34 +76,48 @@ class Classifier(sparkContext: SparkContext, vectorizer: ActorRef, twitterHandle
         .setInputCol("sentiment")
         .setOutputCol("label")
 
+      /**
+       * Params and defaults:
+       *
+       * maxIter: maximum number of iterations (>= 0) (default: 1)
+       * minCount: the minimum number of times a token must appear to be included in the word2vec model's vocabulary (default: 5)
+       * numPartitions: number of partitions for sentences of words (default: 1)
+       * outputCol: output column name (default: w2v_2521010012f2__output, current: features)
+       * seed: random seed (default: -1961189076)
+       * stepSize: Step size to be used for each iteration of optimization. (default: 0.025)
+       * vectorSize: the dimension of codes after transforming from words (default: 100)
+       */
       val word2Vec = new Word2Vec()
         .setInputCol("tokens")
         .setOutputCol("features")
 
+      /**
+       * Params and defaults:
+       *
+       * featuresCol: features column name (default: features)
+       * fitIntercept: whether to fit an intercept term (default: true)
+       * labelCol: label column name (default: label)
+       * maxIter: maximum number of iterations (>= 0) (default: 100)
+       * predictionCol: prediction column name (default: prediction)
+       * probabilityCol: Column name for predicted class conditional probabilities. Note: Not all models output well-calibrated probability estimates! These probabilities should be treated as confidences, not precise probabilities. (default: probability)
+       * rawPredictionCol: raw prediction (a.k.a. confidence) column name (default: rawPrediction)
+       * regParam: regularization parameter (>= 0) (default: 0.0)
+       * standardization: whether to standardize the training features before fitting the model. (default: true)
+       * threshold: threshold in binary classification prediction, in range [0, 1] (default: 0.5)
+       * thresholds: Thresholds in multi-class classification to adjust the probability of predicting each class. Array must have length equal to the number of classes, with values >= 0. The class with largest value p/t is predicted, where p is the original probability of that class and t is the class' threshold. (undefined)
+       * tol: the convergence tolerance for iterative algorithms (default: 1.0E-6)
+       */
       val lr = new LogisticRegression()
 
       val pipeline = new Pipeline()
         .setStages(Array(indexer, linguisticTransformer, tokenizer, word2Vec, lr))
 
-      val paramGrid = new ParamGridBuilder()
-        .addGrid(word2Vec.vectorSize, Array(100, 300))
-        .addGrid(word2Vec.minCount, Array(0, 5))
-        .addGrid(lr.regParam, Array(0.01, 0.1, 1.0))
-        .addGrid(lr.maxIter, Array(10))
-        .build()
-
-      val cv = new CrossValidator()
-        .setNumFolds(10)
-        .setEstimator(pipeline)
-        .setEstimatorParamMaps(paramGrid)
-        .setEvaluator(new BinaryClassificationEvaluator)
-
-      val cvModel = cv.fit(train)
+      val model = pipeline.fit(train)
 
       var total = 0.0
       var correct = 0.0
 
-      cvModel.transform(test)
+      model.transform(test)
         .select("tweet", "sentiment", "label", "probability", "prediction")
         .collect()
         .foreach { case Row(tweet, sentiment, label, prob, prediction) =>
@@ -112,9 +127,10 @@ class Classifier(sparkContext: SparkContext, vectorizer: ActorRef, twitterHandle
         }
 
       val precision = correct / total
+
       log.info(s"precision: ${precision}")
 
-      context.become(predicting(cvModel.bestModel))
+      context.become(predicting(model))
 
       self ! Dequeue
     }
