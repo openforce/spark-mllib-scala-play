7c6dd2fe22133168d16e9aed134c6270eee63540
Klaus Harrer <klaus.harrer@openforce.com>
2015-09-24 17:51:04 +0200

    /predict outputs now json

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,12 +2,13 @@ package controllers
 
 import javax.inject._
 
-import actors.Classifier.{PredictResults, Predict}
+import actors.Classifier.{PredictResult, PredictResults, Predict}
 import actors.Receptionist
 import actors.Receptionist.GetClassifier
 import akka.actor.{ActorRef, ActorSystem}
 import akka.util.Timeout
 import org.apache.spark.SparkContext
+import play.api.libs.json.{Json, JsPath, Writes}
 import play.api.mvc.{Action, Controller}
 import akka.pattern._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
@@ -20,14 +21,14 @@ class Application @Inject() (system: ActorSystem, sparkContext: SparkContext) ex
 
   implicit val timeout = Timeout(10.minutes)
 
-
-
+  implicit val predictResultWrites = Json.writes[PredictResult]
+  implicit val predictResultReads = Json.reads[PredictResult]
 
   def predict(keyword: String) = Action.async {
     for {
       classifier <- (receptionist ? GetClassifier).mapTo[ActorRef]
       predictResults <- (classifier ? Predict(keyword)).mapTo[PredictResults]
-    } yield Ok(predictResults.result.mkString("\n"))
+    } yield Ok(Json.toJson(predictResults.result))
   }
 
   def index = Action {
