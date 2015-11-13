e10f610605dcf32d1aa5fb53e506e957bd0f14b9
Shokunin San <me@flatmap.io>
2015-11-13 17:08:40 +0100

    Refactored statistics generation.

--- app/actors/EventListener.scala
+++ app/actors/EventListener.scala
@@ -1,29 +1,30 @@
 package actors
 
+import actors.StatisticsServer.Statistics
 import akka.actor.{Actor, ActorRef, Props}
-import play.api.libs.json.JsValue
+import play.api.libs.json.Json
 
 object EventListener {
+
   def props(out: ActorRef, eventServer: ActorRef) = Props(new EventListener(out, eventServer))
+
 }
 
 class EventListener(out: ActorRef, eventServer: ActorRef) extends Actor {
 
-  override def preStart() = {
-    eventServer ! Subscribe
-  }
+  override def preStart() = eventServer ! Subscribe
 
   override def postStop(): Unit = {
     super.postStop()
-
     eventServer ! Unsubscribe
   }
 
   def receive = {
-    case msg: String =>
-      out ! msg
 
-    case msg: JsValue =>
-      out ! msg
+    case msg: String => out ! msg
+
+    case msg: Statistics => out ! Json.toJson(msg)
+
   }
+
 }
